32ef3d19841c30314066a8706146c7e77c0eda25
Yash_G.G <157970758+YashGhavghave@users.noreply.github.com>
2024-09-11 20:58:54 +0530

    fgsrh

---
+++ Server.sql
@@ -0,0 +1,8 @@
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+September 11, 2024 - 12:35:21
+Django version 4.x.x, using settings 'myproject.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CONTROL-C.
